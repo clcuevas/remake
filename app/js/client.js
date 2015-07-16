95bb7fdc02e2017be681ceb0d69f753fd8a22d56
Claudia Cuevas <omordn@Claudias-iMac.local>
2015-07-16 09:59:00 -0700

    add files for beginning stages of authentication with angularJS

--- app/js/client.js
+++ app/js/client.js
@@ -2,8 +2,10 @@
 
 require('angular/angular');
 require('angular-route');
+require('angular-cookies');
+require('angular-base64');
 
-var freezrApp = angular.module('freezrApp', ['ngRoute']);
+var freezrApp = angular.module('freezrApp', ['ngRoute', 'ngCookies', 'base64']);
 
 //services
 
@@ -14,3 +16,20 @@ var freezrApp = angular.module('freezrApp', ['ngRoute']);
 //directives
 
 
+//freezrApp route configuration logic
+freezrApp.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/sign_in', {
+      templateUrl: 'templates/views/auth.html',
+      controller: 'authCtrl'
+    });
+}])
+.run(function($rootScope, $location, auth) {
+  $rootScope.$on('$routeChangeStart', function(event, next, current) {
+    if (!auth.isSignedIn()) {
+      if (next.$route.templateUrl !== 'templates/views/auth.html') {
+        $location.path('/sign_in');
+      }
+    }
+  });
+});
