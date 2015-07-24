8486256dfd93e7135a8058d62c1f20b9d00557d9
Claudia Cuevas <omordn@Claudias-iMac.local>
2015-07-24 14:33:10 -0700

    add authentication services script

--- app/js/auth/services/auth_service.js
+++ app/js/auth/services/auth_service.js
@@ -1,1 +1,48 @@
 'use strict';
+
+module.exports = function(app) {
+  app.factory('auth', ['$http', '$base64', '$cookies', function($http, $base64, $cookies) {
+    return {
+      signIn: function(user, callback) {
+        var encoded = $base64.encode(user.email + ':' + user.password);
+
+        $http
+          .get('/api/sign_in', {
+            headers: {'Authorization': 'Basic ' + encoded}
+          })
+          .success(function(data) {
+            $cookies.put('token', data.token);
+            $cookies.put('username', data.username);
+            callback(null);
+          })
+          .error(function(data) {
+            callback(data);
+          });
+      },
+
+      create: function(user, callback) {
+        $http
+          .post('/api/create_user', user)
+          .success(function(data) {
+            $cookies.put('token', data.token);
+            $cookies.put('username', data.username);
+            callback(null);
+          })
+          .error(function(data) {
+            callback(data);
+          });
+      },
+
+      logout: function() {
+        //remove cookie information
+        $cookies.put('token', '');
+        $cookies.put('username', '');
+      },
+
+      isSignedIn: function() {
+        return !!($cookies.get('token') && $cookies.get('token').length);
+      }
+
+    };
+  }]);
+};
